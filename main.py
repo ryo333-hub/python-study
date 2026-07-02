46e2cef08b9a24bf0fca8f3ad6239aa07ae86fd8
Ryo333-hub <2748048713@qq.com>
2026-07-02 21:35:57 +0800

    v2学习记录系统完成

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ print("=== 学习记录系统 ===")
 text = input("请输入你今天学的内容：今天学了GitHub和Python基础")
 
 save_record(text)
+
+from record import save_record
+
+print("=== 学习记录系统 v2 ===")
+
+while True:
+    text = input("请输入学习内容（输入exit退出）")
+
+    if text == "exit":
+        print("已退出系统")
+        break
+
+    save_record(text)
--- record.py
+++ record.py
@@ -3,3 +3,13 @@ def save_record(text):
         f.write(text + "\n")
 
     print("✔ 记录已保存")
+from datetime import datetime
+
+def save_record(text):
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log = time + " | " + text
+
+    with open("log.txt", "a", encoding="utf-8") as f:
+        f.write(log + "\n")
+
+        print("✔ 已记录：", log)
